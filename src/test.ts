a306188fbd25793b4590b4283d6bdbb9c96d2db3
Subramanian Elavathur <subramanian.elavathur@gmail.com>
2021-09-05 19:23:28 +0530

    good-vibes 0.13.0 upgrade

--- src/test.ts
+++ src/test.ts
@@ -10,9 +10,9 @@ const { before, test, after, sync } = group("Glitch DB");
 
 let glitchDB: GlitchDB<string>;
 
-before(async (done, log) => {
+before(async (context) => {
   tempDirectory = path.join(os.tmpdir(), "glitch");
-  log(`Created temp directory for tests at: ${tempDirectory}`);
+  context.log(`Created temp directory for tests at: ${tempDirectory}`);
   const multiDB = new GlitchMultiDB(tempDirectory);
   const additionalKeyGenerator: AdditionalKeyGenerator<string> = (
     key,
@@ -22,29 +22,29 @@ before(async (done, log) => {
   await glitchDB.set("key-1", "value-1");
   await glitchDB.set("key-2", "value-2");
   await glitchDB.set("key-3", "value-3");
-  log(`Glitch DB setup with dummy data complete`);
-  done();
+  context.log(`Glitch DB setup with dummy data complete`);
+  context.done();
 });
 
 sync(); // run tests one after the other
 
-test("get api", async (v) => {
-  v.check("value-1", await glitchDB.get("key-1")).done();
+test("get api", async (c) => {
+  c.check("value-1", await glitchDB.get("key-1")).done();
 });
 
-test("get api with additional keys", async (v) => {
-  v.check("value-2", await glitchDB.get("value-2"))
+test("get api with additional keys", async (c) => {
+  c.check("value-2", await glitchDB.get("value-2"))
     .check("value-2", await glitchDB.get("key-2~value-2"))
     .done();
 });
 
-test("exists api", async (v) => {
-  v.check(true, await glitchDB.exists("key-1")).done();
+test("exists api", async (c) => {
+  c.check(true, await glitchDB.exists("key-1")).done();
 });
 
-test("reset api", async (v) => {
+test("reset api", async (c) => {
   await glitchDB.set("key-1", "value-4");
-  v.check("value-4", await glitchDB.get("value-4"))
+  c.check("value-4", await glitchDB.get("value-4"))
     .check("value-4", await glitchDB.get("key-1~value-4"))
     .check("value-4", await glitchDB.get("key-1"))
     .check(undefined, await glitchDB.get("value-1"))
@@ -52,37 +52,37 @@ test("reset api", async (v) => {
     .done();
 });
 
-test("unset api", async (v) => {
+test("unset api", async (c) => {
   await glitchDB.unset("key-3");
-  v.check(undefined, await glitchDB.get("key-3")).done();
+  c.check(undefined, await glitchDB.get("key-3")).done();
 });
 
-test("keys api", async (v) => {
-  v.check(["key-1", "key-2"], await glitchDB.keys()).done();
+test("keys api", async (c) => {
+  c.check(["key-1", "key-2"], await glitchDB.keys()).done();
 });
 
-test("keys api with additional keys", async (v) => {
-  v.check(
+test("keys api with additional keys", async (c) => {
+  c.check(
     ["key-1", "key-1~value-4", "key-2", "key-2~value-2", "value-2", "value-4"],
     await glitchDB.keys(true)
   ).done();
 });
 
-test("data api", async (v) => {
-  v.check(
+test("data api", async (c) => {
+  c.check(
     { "key-1": "value-4", "key-2": "value-2" },
     await glitchDB.data()
   ).done();
 });
 
-after(async (done, log) => {
+after(async (c) => {
   try {
     await fs.rmdir(tempDirectory, { recursive: true });
-    log("Deleted temp directory after tests");
-    done();
+    c.log("Deleted temp directory after tests");
+    c.done();
   } catch (e) {
-    log(`Could not delete temp directory at: ${tempDirectory}`);
-    done();
+    c.log(`Could not delete temp directory at: ${tempDirectory}`);
+    c.done();
   } finally {
     tempDirectory = "";
   }
